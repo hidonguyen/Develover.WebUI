10d0e0191c5b76b33c4d0ed9cf4bfd5cfb49e3df
Hoang Nhu <hoangng@unit.com.vn>
2021-02-26 14:41:58 +0700

    Document type done

---
+++ Controllers/DocumentTypeController.cs
@@ -0,0 +1,54 @@
+﻿using Develover.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Develover.WebUI.Controllers
+{
+    public class DocumentTypeController : Controller
+    {
+        private readonly ILogger<DocumentTypeController> _logger;
+
+        public DocumentTypeController(ILogger<DocumentTypeController> logger)
+        {
+            _logger = logger;
+        }
+
+        private DocumentTypeViewModel model = new DocumentTypeViewModel() { Id = Guid.NewGuid(), Name = "Test", Status = true };
+
+        [HttpGet]
+        public async Task<IActionResult> GetList()
+        {
+            List<DocumentTypeViewModel> result = new List<DocumentTypeViewModel>
+            {
+                model
+            };
+
+            return Json(new { data = result });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetModel(string id)
+        {
+
+            return Json(new { model });
+        }
+
+        [HttpGet]
+        public IActionResult Index() => View();
+
+        [HttpGet]
+        public IActionResult New() => View(nameof(Detail));
+
+        [HttpGet]
+        public IActionResult Detail(string id = "") => View();
+
+        [HttpPost]
+        public IActionResult Save(DocumentTypeViewModel model) => Ok();
+
+        [HttpPost]
+        public IActionResult Delete(string id) => Ok();
+    }
+}
