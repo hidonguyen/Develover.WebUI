598aaa14ae162717448273e4054a7af43cebd167
hoangNagi <36804847+HoangNagi@users.noreply.github.com>
2021-02-28 12:48:28 +0700

    Allowance Deduction Type done

---
+++ Controllers/AllowanceDeductionTypeController.cs
@@ -0,0 +1,54 @@
+﻿using Develover.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Develover.WebUI.Controllers
+{
+    public class AllowanceDeductionTypeController : Controller
+    {
+        private readonly ILogger<AllowanceDeductionTypeController> _logger;
+
+        public AllowanceDeductionTypeController(ILogger<AllowanceDeductionTypeController> logger)
+        {
+            _logger = logger;
+        }
+
+        private AllowanceDeductionTypeViewModel model = new AllowanceDeductionTypeViewModel() { Id = Guid.NewGuid(), Name = "Test", Status = true };
+
+        [HttpGet]
+        public async Task<IActionResult> GetList()
+        {
+            List<AllowanceDeductionTypeViewModel> result = new List<AllowanceDeductionTypeViewModel>
+            {
+                model
+            };
+
+            return Json(new { data = result });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetModel(string id)
+        {
+
+            return Json(new { model });
+        }
+
+        [HttpGet]
+        public IActionResult Index() => View();
+
+        [HttpGet]
+        public IActionResult New() => View(nameof(Detail));
+
+        [HttpGet]
+        public IActionResult Detail(string id = "") => View();
+
+        [HttpPost]
+        public IActionResult Save(AllowanceDeductionTypeViewModel model) => Ok();
+
+        [HttpPost]
+        public IActionResult Delete(string id) => Ok();
+    }
+}
---
+++ Models/Catalog/AllowanceDeductionTypeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Develover.WebUI.Models
+{
+    public class AllowanceDeductionTypeViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool ShowOnPayslip { get; set; }
+        public bool IsDeduction { get; set; }
+        public bool Status { get; set; }
+    }
+}
