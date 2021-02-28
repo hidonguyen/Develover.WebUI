3b8d62820cfc895dbbe0e4525f67e1baf6e99041
hoangNagi <36804847+HoangNagi@users.noreply.github.com>
2021-02-28 22:23:28 +0700

    KPI Type done

---
+++ Models/Catalog/KPITypeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Develover.WebUI.Models
+{
+    public class KPITypeViewModel
+    {
+        public Guid Id { get; set; }
+        public DateTime EffectiveDate { get; set; }
+        public string Value0 { get; set; }
+        public string Value1 { get; set; }
+        public string Value2 { get; set; }
+        public string Value3 { get; set; }
+        public string Value4 { get; set; }
+        public string Value5 { get; set; }
+    }
+}
