49f105587539a329c1f5992611da6b23e5090315
hoangNagi <36804847+HoangNagi@users.noreply.github.com>
2021-02-28 14:37:51 +0700

    KPI Registration done

---
+++ Models/Catalog/KPIRegistrationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Develover.WebUI.Models
+{
+    public class KPIRegistrationViewModel
+    {
+        public Guid Id { get; set; }
+        public string DivisionId { get; set; }
+        public string KPIType { get; set; }
+        public DateTime IssueDate { get; set; }
+    }
+}
