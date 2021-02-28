c8aed449da8236a65f4326ca2e1b257a5abb3eac
hoangNagi <36804847+HoangNagi@users.noreply.github.com>
2021-02-28 17:21:36 +0700

    Department transfer done

---
+++ Models/Catalog/DepartmentTransferViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Develover.WebUI.Models
+{
+    public class DepartmentTransferViewModel
+    {
+        public Guid Id { get; set; }
+        public Guid EmployeeId { get; set; }
+        public Guid DivisionId { get; set; }
+        public string No { get; set; }
+        public string EffectiveDate { get; set; }
+        public string Note { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
